fc3d6b21d328e793d151a9052e74a5e1526cc95b
r89 <r89@r89desktop>
2021-04-16 11:45:33 +0600

    Update package-lock.json, package.json, and 7 more files...

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,77 @@
 <script>
-	export let name;
+	import { Route, active } from "tinro";
+
+	import RemoteTemplates from "./pages/RemoteTemplates.svelte";
 </script>
 
-<main>
-	<h1>He6llo {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorialddgg</a> to
-		learn how to build Svdelte apps.
-	</p>
-</main>
+<div class="navbar">
+	<a href="/" use:active exact>
+		<img src="assets/icons/cloud.svg" alt="remote" /></a
+	>
+	<a href="/make" use:active exact
+		><img src="assets/icons/prepare.svg" alt="prepare" /></a
+	>
+	<a href="/local" use:active exact
+		><img src="assets/icons/forest.svg" alt="forest" /></a
+	>
+</div>
+
+<Route path="/"><RemoteTemplates /></Route>
 
 <style>
-	main {
+	.navbar {
+		width: 100%;
+		display: block;
+		bottom: 0;
+		position: fixed;
+		background-color: #1e1e1e;
+	}
+	.navbar a {
+		float: left;
+		padding-top: 3px;
+		padding-bottom: 5px;
+		color: white;
+		text-decoration: none;
+		font-size: 17px;
+		width: 33.3%; /* Four links of equal widths */
+		text-align: center;
+		align-content: center;
+	}
+	.navbar a:hover {
+		background-color: lightblue;
+	}
+	img {
+		max-height: 1.5em;
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	:global(.active) {
+		background-color: lightblue;
+	}
+	/* main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	} */
+	:global(body) {
+		background-color: #1e1e1e;
+		color: #ff7f50;
+		font-size: 1em;
+		line-height: 1.5;
 	}
-
-	h1 {
+	/* h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
+	} */
 
-	@media (min-width: 640px) {
+	/* @media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
-	}
+	} */
 </style>
