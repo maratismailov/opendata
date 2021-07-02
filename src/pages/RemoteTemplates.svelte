664684c4bc06d9d52a0b501ffcd81c7d65b93368
r89 <r89@r89rig>
2021-07-02 16:02:08 +0600

    Добавлен индикатор загрузки опроса, индикатор готовности и индикатор ошибки с кодами ответа сервера

--- src/pages/RemoteTemplates.svelte
+++ src/pages/RemoteTemplates.svelte
@@ -1,8 +1,7 @@
 <script>
     import { onMount } from "svelte";
 
-    import { init_db } from '../init_db'
-
+    import { init_db } from "../init_db";
 
     export let server_url;
     export let dictionary;
@@ -12,34 +11,37 @@
     let db;
     let object_code = "";
     let active_template = "";
+    let is_loading = false;
+    let is_ready = false;
+    let is_error = false;
+    let error_msg = "";
 
     onMount(async () => {
         let server = server_url + `/get_templates_list`;
         const res = await fetch(server);
         const result = await res.json();
         templates_list = JSON.parse(result);
-        init_db()
+        init_db();
         var db_mobilesurvey = indexedDB.open("db_mobilesurvey", 1);
         db_mobilesurvey.onsuccess = function (e) {
             db = e.target.result;
-            console.log(db)
         };
-        console.log(db)
     });
     const get_initial_fields = async (id) => {
         active_template = id;
         let server = server_url + `/get_initial_fields?id=` + id;
         const res = await fetch(server);
         const result = await res.json();
-        console.log(JSON.parse(result).initial_fields);
         initial_fields = await JSON.parse(result).initial_fields;
-        console.log(initial_fields);
         initial_fields_values = initial_fields.map((elem) => {
             elem.value = "";
             return elem;
         });
     };
     const generate_survey = async (id) => {
+        error_msg = ''
+        is_error = false
+        is_loading = true;
         initial_fields_values.map((elem) => {
             object_code = object_code + elem.value + "-";
         });
@@ -48,8 +50,12 @@
         let server2 = server_url + `/generate_survey?id=` + id + "&values=" + JSON.stringify(initial_fields_values);
         let mbtiles_url = server_url + `/generate_mbtiles?id=` + id + "&values=" + JSON.stringify(initial_fields_values);
 
-        const pre_objects = await fetch(server).then((response) => response.json());
-        const pre_survey = await fetch(server2).then((response) => response.json());
+        const pre_objects1 = await fetch(server);
+        const pre_objects = await pre_objects1.json();
+        const pre_objects_status = await pre_objects1.status;
+        const pre_survey1 = await fetch(server2);
+        const pre_survey = await pre_survey1.json();
+        const pre_survey_status = await pre_survey1.status;
         const survey = JSON.parse(pre_survey);
         const object_id = survey.survey_body.object_code;
         const pre_objects2 = JSON.parse(pre_objects);
@@ -65,12 +71,25 @@
         const survey_name = survey.survey_body.name + " " + object_code;
         survey.objects = objects;
         survey.survey_name = survey_name;
-        const mbitles = await fetch(mbtiles_url).then((response) => response.blob().then((blob) => save_mbtiles(blob, survey_name)));
+        const mbtiles1 = await fetch(mbtiles_url);
+        const mbtiles = await mbtiles1.blob().then((blob) => save_mbtiles(blob, survey_name));
+        console.log(mbtiles1)
+        const mbtiles_status = await mbtiles1.status;
         save_survey(survey, survey_name);
+        if (pre_objects_status == "200" && pre_survey_status == "200" && mbtiles_status == "200") {
+            is_loading = false;
+            is_ready = true;
+        } else {
+            is_error = true;
+            is_loading = false;
+            error_msg = pre_objects_status + "\n" + pre_survey_status + "\n" + mbtiles_status;
+        }
+        setTimeout(() => {
+            is_ready = false;
+        }, 5000);
     };
 
     const save_mbtiles = (file, survey_name) => {
-        console.log(file);
         var transaction = db.transaction(["mbtiles"], "readwrite");
         var store = transaction.objectStore("mbtiles");
 
@@ -123,7 +142,22 @@
         {/each}
     {/if}
 
-    <button on:click={() => console.log(db)}>test db</button>
+    <!-- <button on:click={() => console.log(db)}>test db</button> -->
+
+    {#if is_loading}
+        <img src="assets/icons/loader.svg" alt="loading" style="max-height: 5em" />
+    {/if}
+    {#if is_ready}
+        <img src="assets/icons/ready.svg" alt="ready" style="max-height: 5em" />
+    {/if}
+    {#if is_error}
+        <img src="assets/icons/error.svg" alt="error" style="max-height: 5em" />
+    {/if}
+    {#if error_msg}
+        <div>
+            {error_msg}
+        </div>
+    {/if}
 
     <!-- {/if} -->
 </div>
